0625ff93113ddc2e847acb0efa20500c5c3c820a
Tony <tony@tony-desktop.home>
2011-10-02 18:40:48 -0400

    cleared stage 1 of hmm moving onto stage 2 shortlyish... noticing difficulties dealing with floating point numbers

--- src/hmm/markov_model.go
+++ src/hmm/markov_model.go
@@ -2,8 +2,34 @@ package hmm
 
 import (
 	"fmt";
+	"rand";
 )
 
 func Printmm() {
 	fmt.Printf("second print out\n")
+}
+
+// this function is the first iteration for the states to be learned in the hmm
+// the current probabilities should be as follows:
+// 0 = 15%
+// 1 = 20%
+// 2 = 30%
+// 3 = 35%
+func Getnextstate() int {
+	r := rand.Intn(100) // it is worth noting that this function will return 0 but will not return 100
+	
+	ret := -1
+	
+	switch {
+		case r < 15:
+			ret = 0
+		case r < 35:
+			ret = 1
+		case r < 65:
+			ret = 2
+		default:
+			ret = 3
+	}
+
+	return ret
 }
--- src/main/main.go
+++ src/main/main.go
@@ -3,9 +3,45 @@ package main
 import (
 	"fmt";
 	"hmm";
+	"strconv";
 )
 
+/*
+For this main function we will be making calls to other functions which can be used to test individual components.
+The main function should be sectioned off into two parts. The top part should be used to call test functions. The bottom part should be used for code for the project as a whole.
+This setup will allow us to quickly and easily comment out unwanted code to test/build only the relevant code.
+Either section may be commented out by removing the first / at the beginning of the seciton.
+Likewise the code can be restored by replacing the first / at the beginning of the section (that is to say, /* for commented out and //* for commented in).
+*/
+
+
+func test_atomaton() {
+	// TODO: this function should be converted over to using floating point arithmatic a) to show how it is done and b) to absolutely verify that it is working
+	counters := make([]int, 4)
+	numruns := 10000
+	
+	for i := 0; i < len(counters); i++ {
+		counters[i] = 0
+	}
+
+	for i := 0; i < numruns; i++ {
+		counters[hmm.Getnextstate()]++
+	}
+
+	for i := 0; i < len(counters); i++ {
+		fmt.Printf("state " + strconv.Itoa(i) + " percentage = " + strconv.Itoa((counters[i] * 100)/numruns) + "\n")
+	}
+}
+
 func main() {
-	fmt.Printf("first print\n")
-	hmm.Printmm()
+//********************************************
+// this is the section for test code
+
+	test_atomaton()
+
+//********************************************/
+//********************************************
+// this is for the final project
+
+//********************************************/
 }
