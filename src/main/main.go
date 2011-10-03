3016af9619a6feab94cb7515512402064431a496
Tony <tony@tony-desktop.home>
2011-10-02 20:12:06 -0400

    updated to use floats in test_atomaton... :)

--- src/main/main.go
+++ src/main/main.go
@@ -12,24 +12,24 @@ The main function should be sectioned off into two parts. The top part should be
 This setup will allow us to quickly and easily comment out unwanted code to test/build only the relevant code.
 Either section may be commented out by removing the first / at the beginning of the seciton.
 Likewise the code can be restored by replacing the first / at the beginning of the section (that is to say, /* for commented out and //* for commented in).
+Naming convention: Test functions should have "test_" at the front of them.
 */
 
 
 func test_atomaton() {
-	// TODO: this function should be converted over to using floating point arithmatic a) to show how it is done and b) to absolutely verify that it is working
-	counters := make([]int, 4)
-	numruns := 10000
+	counters := make([]float64, 4)
+	numruns := 10000.0
 	
 	for i := 0; i < len(counters); i++ {
-		counters[i] = 0
+		counters[i] = 0.0
 	}
 
-	for i := 0; i < numruns; i++ {
+	for i := 0.0; i < numruns; i++ {
 		counters[hmm.Getnextstate()]++
 	}
 
 	for i := 0; i < len(counters); i++ {
-		fmt.Printf("state " + strconv.Itoa(i) + " percentage = " + strconv.Itoa((counters[i] * 100)/numruns) + "\n")
+		fmt.Printf("state " + strconv.Itoa(i) + " percentage = " + strconv.Ftoa64((counters[i] * 100)/numruns, 'f', 5) + "\n")
 	}
 }
 
